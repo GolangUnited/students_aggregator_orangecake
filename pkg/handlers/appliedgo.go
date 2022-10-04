8e101a29dbe68f8017184048bb06220b7a9cc030
Yahor Bagatyrevich <egorbgtrvch@gmail.com>
2022-10-04 23:21:10 +0300

    Added async usage, corrected struct names

--- pkg/handlers/appliedgo.go
+++ pkg/handlers/appliedgo.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Article struct {
-	Title       string
+	Caption     string
 	PublishDate string
 	Description string
 	Link        string
@@ -21,27 +21,27 @@ func parseAppliedGo() []Article {
 	var articlesList []Article
 
 	// This parser is used to scrap the data from every article's personal page
-	articleParser := colly.NewCollector()
+	articleParser := colly.NewCollector(colly.Async(true))
 	articleParser.OnHTML("head", func(e *colly.HTMLElement) {
 		name := e.ChildAttr("meta[property=\"og:title\"]", "content")
 		descr := e.ChildAttr("meta[name=\"description\"]", "content")
 		url := e.ChildAttr("meta[property=\"og:url\"]", "content")
 		publicationTimeStr := e.ChildAttr("meta[property=\"article:published_time\"]", "content")
 		newArticle := Article{
-			Title:       name,
+			Caption:     name,
 			Description: descr,
 			Link:        url,
 			PublishDate: strings.TrimSpace(publicationTimeStr),
 		}
 
-		// In case of async usage of gocolly, I put a mutex here to prevent data race
+		// Because of async usage of gocolly, I put a mutex here to prevent data race
 		listMutex.Lock()
 		articlesList = append(articlesList, newArticle)
 		listMutex.Unlock()
 	})
 
 	// This parser is used to scrap all the articles from the page with list of them
-	articleCollector := colly.NewCollector()
+	articleCollector := colly.NewCollector(colly.Async(true))
 	articleCollector.OnHTML("header[class=\"article-header\"] > a", func(e *colly.HTMLElement) {
 		// hasn't made error handling in func Visit yet, because don't know what to do with this mistakes
 		articleParser.Visit(e.Attr("href"))
