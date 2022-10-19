14ef6a2b920dae7acfd465b652c1ee83e03a2fc8
Armvi <72762240+Armvi@users.noreply.github.com>
2022-10-19 11:29:02 +0300

    Update errors.go

--- pkg/errors.go
+++ pkg/errors.go
@@ -8,7 +8,7 @@ var (
 	// ErrRequiredFieldNotFound use this error when you can't get data for Title or URL
 	ErrRequiredFieldNotFound = errors.New("data for required field not found")
 
-	// ErrFieldNotFound use this error when you can't get data for Author, Data or Description
+	// ErrFieldNotFound use this error when you can't get data for Author, Date or Description
 	ErrFieldNotFound = errors.New("data for field not found")
 
 	// ErrHTMLAccess return this error from GetArticles if you can't get access to you web-page otherwise return nil error and articles which you were able to get
