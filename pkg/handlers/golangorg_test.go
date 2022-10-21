4441b4d758d099cbbeb7d36d874347a72ed6a44c
Artem G <itemun@gmail.com>
2022-10-21 11:24:51 +0300

    Changing values of PublishDate field

--- pkg/handlers/golangorg_test.go
+++ pkg/handlers/golangorg_test.go
@@ -33,53 +33,38 @@ func TestGolangorg(t *testing.T) {
 	// create expected data
 	lExpectedData := []core.Article{
 		{
-			Title:  "Go runtime: 4 years later",
-			Author: "Michael Knyszek",
-			Link:   "https://tip.golang.org/blog/all/go119runtime",
-			PublishDate: func() time.Time {
-				t, _ := time.Parse("_2 January 2006", "26 September 2022")
-				return t.UTC()
-			}(),
+			Title:       "Go runtime: 4 years later",
+			Author:      "Michael Knyszek",
+			Link:        "https://tip.golang.org/blog/all/go119runtime",
+			PublishDate: time.Date(2022, time.September, 26, 0, 0, 0, 0, time.UTC),
 			Description: "A check-in on the status of Go runtime development",
 		},
 		{
-			Title:  "Go Developer Survey 2022 Q2 Results",
-			Author: "Todd Kulesza",
-			Link:   "https://tip.golang.org/blog/all/survey2022-q2-results",
-			PublishDate: func() time.Time {
-				t, _ := time.Parse("_2 January 2006", "8 September 2022")
-				return t.UTC()
-			}(),
+			Title:       "Go Developer Survey 2022 Q2 Results",
+			Author:      "Todd Kulesza",
+			Link:        "https://tip.golang.org/blog/all/survey2022-q2-results",
+			PublishDate: time.Date(2022, time.September, 8, 0, 0, 0, 0, time.UTC),
 			Description: "An analysis of the results from the 2022 Q2 Go Developer Survey.",
 		},
 		{
-			Title:  "Vulnerability Management for Go",
-			Author: "Julie Qiu, for the Go security team",
-			Link:   "https://tip.golang.org/blog/all/vuln",
-			PublishDate: func() time.Time {
-				t, _ := time.Parse("_2 January 2006", "6 September 2022")
-				return t.UTC()
-			}(),
+			Title:       "Vulnerability Management for Go",
+			Author:      "Julie Qiu, for the Go security team",
+			Link:        "https://tip.golang.org/blog/all/vuln",
+			PublishDate: time.Date(2022, time.September, 6, 0, 0, 0, 0, time.UTC),
 			Description: "Announcing vulnerability management for Go, to help developers learn about known vulnerabilities in their dependencies.",
 		},
 		{
-			Title:  "Go 1.19 is released!",
-			Author: "The Go Team",
-			Link:   "https://tip.golang.org/blog/all/go1.19",
-			PublishDate: func() time.Time {
-				t, _ := time.Parse("_2 January 2006", "2 August 2022")
-				return t.UTC()
-			}(),
+			Title:       "Go 1.19 is released!",
+			Author:      "The Go Team",
+			Link:        "https://tip.golang.org/blog/all/go1.19",
+			PublishDate: time.Date(2022, time.August, 2, 0, 0, 0, 0, time.UTC),
 			Description: "Go 1.19 adds richer doc comments, performance improvements, and more.",
 		},
 		{
-			Title:  "Share your feedback about developing with Go",
-			Author: "Todd Kulesza",
-			Link:   "https://tip.golang.org/blog/all/survey2022-q2",
-			PublishDate: func() time.Time {
-				t, _ := time.Parse("_2 January 2006", "1 June 2022")
-				return t.UTC()
-			}(),
+			Title:       "Share your feedback about developing with Go",
+			Author:      "Todd Kulesza",
+			Link:        "https://tip.golang.org/blog/all/survey2022-q2",
+			PublishDate: time.Date(2022, time.June, 1, 0, 0, 0, 0, time.UTC),
 			Description: "Help shape the future of Go by sharing your thoughts via the Go Developer Survey",
 		},
 	}
