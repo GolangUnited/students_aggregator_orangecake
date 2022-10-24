a2ff57ab6f537dd1df4bae80e5bc4fe1585b8dcf
semyon4eg <semenowav@gmail.com>
2022-10-24 09:52:03 +0200

    Adds more test cases

--- pkg/handlers/devto.go
+++ pkg/handlers/devto.go
@@ -30,6 +30,7 @@ func NewHandler(aURL string) DevtoHandler {
     return DevtoHandler{URL: aURL, Articles: make([]core.Article, 0), Colly: colly.NewCollector()}
 }
 
+// Initializes new Handler and runs scrapping of URL provided
 func Run() []core.Article {
     lH := NewHandler(Go_URL)
     lH.Scrap()
@@ -37,6 +38,7 @@ func Run() []core.Article {
     return lH.Articles
 }
 
+// Runs scrapping and fills Handler field with Articles info
 func (aHandler *DevtoHandler) Scrap() error {
 
     lArticle := core.Article{}
@@ -47,9 +49,10 @@ func (aHandler *DevtoHandler) Scrap() error {
 
                 lArticle.Author = strings.TrimSpace(h.ChildText(Author_class))
                 if len(lArticle.Author) <= 0 {
-                    fmt.Printf("No author for an Article")
+                    fmt.Printf("No author for an Article\n")
                 }
 
+                // Title is a requiered field
                 lArticle.Title = strings.TrimSpace(h.ChildText(Title_class))
                 if len(lArticle.Title) <= 0 {
                     aHandler.Err = errors.New("no title found for an article - quitting")
@@ -60,9 +63,10 @@ func (aHandler *DevtoHandler) Scrap() error {
                 var lErr error
                 lArticle.PublishDate, lErr = core.ParseDate(time.RFC3339, lDate)
                 if lErr != nil {
-                    fmt.Printf("date cannot be parsed: %s", lErr.Error())
+                    fmt.Printf("date cannot be parsed: %s\n", lErr.Error())
                 }
 
+                // Link is a required field
                 lArticle.Link = Devto_URL + h.ChildAttr("a", "href")
                 if len(lArticle.Link) <= 0 {
                     aHandler.Err = errors.New("no link found for an article - quitting")
@@ -84,7 +88,12 @@ func (aHandler *DevtoHandler) Scrap() error {
 
     lErr := aHandler.Colly.Visit(aHandler.URL)
     if lErr != nil {
-        return errors.New("Error scrapping " + aHandler.URL + "\n\n")
+        return errors.New("error scrapping " + aHandler.URL)
+    }
+    
+    // Checking Handler for any Articles errors
+    if aHandler.Err != nil {
+        return aHandler.Err
     }
 
     return nil
--- pkg/handlers/devto_test.go
+++ pkg/handlers/devto_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"testing"
+    "errors"
     "time"
 
 	"github.com/gocolly/colly"
@@ -11,7 +12,7 @@ import (
 )
 
 const (
-	TestDataFolder = "./test_data/"
+	TestDataFolder = "./test_data/devto/"
 )
 
 func NewTestHandler(aURL string) DevtoHandler {
@@ -28,7 +29,7 @@ func NewTestHandler(aURL string) DevtoHandler {
     return DevtoHandler{URL: aURL, Articles: make([]core.Article, 0), Colly: lScrapper}
 }
 
-func TestGetArticlesListAppliedGo(t *testing.T) {
+func TestScrapDevto(t *testing.T) {
 
     lHandle := NewTestHandler("file://./Devto.html")
     lHandle.Scrap()
@@ -54,4 +55,67 @@ func TestGetArticlesListAppliedGo(t *testing.T) {
         )
 
     assert.Equal(t, lExpectedArticles, lHandle.Articles, "ArticlesEqual")
+}
+
+func TestScrapDevtoEmptyNotRequiredFields(t *testing.T) {
+
+    lHandle := NewTestHandler("file://./DevtoEmptyFields.html")
+    lHandle.Scrap()
+
+	lExpectedArticles := 
+        []core.Article(
+            []core.Article{
+                {
+                    Title:"Restful API with Golang practical approach",
+                    Author:"",
+                    Link:"file://./Article1.html",
+                    PublishDate:time.Date(2022, time.October, 17, 0, 0, 0, 0, time.UTC),
+                    Description:"In this tutorial, we would be creating a Restful API with a practical approach of clean architecture and native Golang without any frameworks.",
+                },
+                {
+                    Title:"Learn Go in Minutes",
+                    Author:"Ayoub Ali",
+                    Link:"file://./Article2.html",
+                    PublishDate: core.NormalizeDate(time.Now()),
+                    Description:"\nGo is an open source programming language supported by Google.\nEasy to learn and get started with.\nBuilt-in concurrency and a robust standard library.\nGrowing ecosystem of partners, communities, and tools.\n",
+                },
+            },
+        )
+
+    assert.Equal(t, lExpectedArticles, lHandle.Articles, "ArticlesEqual")
+}
+
+func TestScrapDevtoEmptyTitle(t *testing.T) {
+
+    var lErr error
+	var lExpectedErr = errors.New("no title found for an article - quitting")
+
+    lHandle := NewTestHandler("file://./DevtoEmptyTitle.html")
+    lErr = lHandle.Scrap()
+
+	assert.Equal(t, lExpectedErr, lErr, "ErrorsEqual")
+}
+
+func TestScrapDevtoEmptyLink(t *testing.T) {
+
+    var lErr error
+	var lExpectedErr = errors.New("no link found for an article - quitting")
+
+    lHandle := NewTestHandler("file://./DevtoEmptyLink.html")
+    lErr = lHandle.Scrap()
+
+	assert.Equal(t, lExpectedErr, lErr, "ErrorsEqual")
+}
+
+func TestScrapDevtoBadUrl(t *testing.T) {
+
+    lBadUrl := "https://de.vto/t/go"
+
+    var lErr error
+	var lExpectedErr = errors.New("error scrapping " + lBadUrl)
+
+    lHandle := NewHandler(lBadUrl)
+    lErr = lHandle.Scrap()
+
+	assert.Equal(t, lExpectedErr, lErr, "ErrorsEqual")
 }
