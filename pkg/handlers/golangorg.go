c5b8400bba953519f1e59dfa19f229efe5ace7b7
Artem G <itemun@gmail.com>
2022-10-26 18:13:53 +0300

    Rewrite scraper with goquery

--- pkg/handlers/golangorg.go
+++ pkg/handlers/golangorg.go
@@ -3,67 +3,61 @@ package handlers
 import (
 	"errors"
 	"fmt"
-	"github.com/gocolly/colly"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/indikator/aggregator_orange_cake/pkg/core"
+	"net/http"
 	"strings"
 )
 
 const (
-	TGO_DOMAIN     = "tip.golang.org"
-	TGO_WWW_DOMAIN = "www.tip.golang.org"
-	TGO_FULL_URL   = "https://tip.golang.org/blog/all"
+	TGO_FULL_URL = "https://tip.golang.org/blog/all"
+	TGO_SURL     = "https://tip.golang.org"
 )
 
 // GolangorgScraper takes data from tip.golang.com/blog/all and converts it into a structure of json.
 func GolangorgScraper(aURL string) ([]core.Article, error) {
 
 	lArticles := make([]core.Article, 0, 0)
-	lDescriptions := make([]string, 0, 0)
 
-	//Create a new collector
-	lC := colly.NewCollector()
-
-	// Call the OnHTML method to take needed data from website
-	lC.OnHTML("p.blogtitle", func(h *colly.HTMLElement) {
-		lT, lErr := core.ParseDate("_2 January 2006", h.ChildText("span.date"))
-
-		if lErr != nil {
-			// TODO: write error to log
-			fmt.Println(errors.New("date can't be formatted: "), lErr)
-			return
-		}
-
-		article := core.Article{
-			Link:        TGO_FULL_URL + h.ChildAttr("a", "href"),
-			Title:       h.ChildText("a[href]"),
-			PublishDate: lT.UTC(),
-			Author:      h.ChildText("span.author"),
-		}
-		lArticles = append(lArticles, article)
-	})
-
-	// Call the OnHTML method secondly with another selector, because I couldn't use two different selectors in OnHTML func
-	lC.OnHTML("p.blogsummary", func(h *colly.HTMLElement) {
-		lDescription := strings.TrimSpace(h.Text)
-		lDescriptions = append(lDescriptions, lDescription)
-	})
-
-	// Before making a request print "Visiting"
-	lC.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
-
-	// Start scraping on URL
-	lErr := lC.Visit(aURL)
+	resp, lErr := http.Get(TGO_FULL_URL)
 	if lErr != nil {
 		// TODO: write error to log
-		return nil, lErr
+		fmt.Println(errors.New("http request returns an error: "), lErr)
 	}
 
-	// Add value to summary field into the Article struct
-	for i := 0; i <= len(lArticles)-1; i++ {
-		lArticles[i].Description = lDescriptions[i]
+	defer resp.Body.Close()
+
+	if resp.StatusCode > 400 {
+		fmt.Println("Status code: ", resp.StatusCode)
 	}
+
+	//rewrite the code with goquery, without goColly
+	doc, lErr := goquery.NewDocumentFromReader(resp.Body)
+	if lErr != nil {
+		// TODO: write error to log
+		fmt.Println(errors.New("goquery.NewDocumentFromReader returns an error: "), lErr)
+	}
+
+	doc.Find("p.blogtitle, p.blogsummary").Each(func(i int, selection *goquery.Selection) {
+		if i%2 == 0 {
+			lLink, _ := selection.Find("a").Attr("href")
+			lT, lErr := core.ParseDate("_2 January 2006", selection.Find("span.date").Text())
+			if lErr != nil {
+				// TODO: write error to log
+				fmt.Println(errors.New("error of ParseDate function: "), lErr)
+			}
+
+			lArticle := core.Article{
+				Title:       selection.Find("a[href]").Text(),
+				Author:      selection.Find("span.author").Text(),
+				PublishDate: lT,
+				Link:        TGO_SURL + lLink,
+				Description: strings.TrimSpace(selection.NextFiltered("p.blogsummary").Text()),
+			}
+
+			lArticles = append(lArticles, lArticle)
+		}
+	})
 
 	return lArticles, nil
 
