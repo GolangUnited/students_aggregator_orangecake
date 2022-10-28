ebcec6f5f4e4fad7b14142edfaa30f98b544193e
Artem G <itemun@gmail.com>
2022-10-28 12:47:48 +0300

    change function to method of struct

--- pkg/handlers/golangorg.go
+++ pkg/handlers/golangorg.go
@@ -10,18 +10,35 @@ import (
 )
 
 const (
-	TGO_SURL = "https://tip.golang.org"
+	GOLANG_ORG_FULL_URL = "https://tip.golang.org/blog/all"
+	GOLANG_ORG_URL      = "https://tip.golang.org"
 )
 
-// GolangorgScraper takes data from tip.golang.com/blog/all and converts it into a structure of json.
-func GolangorgScraper(aURL string) ([]core.Article, error) {
+type GolangOrgHandler struct {
+	url      string
+	articles []core.Article
+	err      error
+}
 
+func NewGolangOrgHandler(aURL string) GolangOrgHandler {
+	return GolangOrgHandler{url: aURL, articles: make([]core.Article, 0)}
+}
+
+func (h *GolangOrgHandler) Run() []core.Article {
+	h.GolangOrgScraper()
+	return h.articles
+}
+
+// GolangOrgScraper takes data from tip.golang.com/blog/all and converts it into a structure of json.
+func (h *GolangOrgHandler) GolangOrgScraper() ([]core.Article, error) {
+
+	lArticle := core.Article{}
 	lArticles := make([]core.Article, 0, 0)
 
-	resp, lErr := http.Get(aURL)
+	resp, lErr := http.Get(GOLANG_ORG_FULL_URL)
 	if lErr != nil {
 		// TODO: write error to log
-		fmt.Println(errors.New("http request returns an error: "), lErr)
+		h.err = errors.New("http request returns an error: ")
 	}
 
 	defer resp.Body.Close()
@@ -34,7 +51,7 @@ func GolangorgScraper(aURL string) ([]core.Article, error) {
 	doc, lErr := goquery.NewDocumentFromReader(resp.Body)
 	if lErr != nil {
 		// TODO: write error to log
-		fmt.Println(errors.New("goquery.NewDocumentFromReader returns an error: "), lErr)
+		h.err = errors.New("goquery.NewDocumentFromReader returns an error: ")
 	}
 
 	doc.Find("p.blogtitle, p.blogsummary").Each(func(i int, selection *goquery.Selection) {
@@ -43,15 +60,36 @@ func GolangorgScraper(aURL string) ([]core.Article, error) {
 			lT, lErr := core.ParseDate("_2 January 2006", selection.Find("span.date").Text())
 			if lErr != nil {
 				// TODO: write error to log
-				fmt.Println(errors.New("error of ParseDate function: "), lErr)
+				h.err = errors.New("error of ParseDate function: ")
 			}
 
-			lArticle := core.Article{
-				Title:       selection.Find("a[href]").Text(),
-				Author:      selection.Find("span.author").Text(),
-				PublishDate: lT,
-				Link:        TGO_SURL + lLink,
-				Description: strings.TrimSpace(selection.NextFiltered("p.blogsummary").Text()),
+			//title is a required field
+			lArticle.Title = selection.Find("a[href]").Text()
+			if len(lArticle.Title) == 0 {
+				// TODO: write error to log
+				h.err = errors.New("title not found in article ==> exit")
+				//how to return h.err
+			}
+
+			lArticle.Link = GOLANG_ORG_URL + lLink
+			if len(lLink) == 0 {
+				// TODO: write error to log
+				h.err = errors.New("link not found in article ==> exit")
+				//how to return h.err
+			}
+
+			lArticle.Author = selection.Find("span.author").Text()
+			if len(lArticle.Author) == 0 {
+				// TODO: write error to log
+				h.err = errors.New("author not found in article")
+			}
+
+			lArticle.PublishDate = lT
+
+			lArticle.Description = strings.TrimSpace(selection.NextFiltered("p.blogsummary").Text())
+			if len(lArticle.Description) == 0 {
+				// TODO: write error to log
+				h.err = errors.New("description not found in article")
 			}
 
 			lArticles = append(lArticles, lArticle)
