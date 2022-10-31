2471b13551739aa2bd18f138d35655ac3930e6ad
Artem G <itemun@gmail.com>
2022-10-31 16:00:24 +0300

    Add Max's desision and edit code with his recommendation

--- pkg/handlers/golangorg.go
+++ pkg/handlers/golangorg.go
@@ -10,8 +10,7 @@ import (
 )
 
 const (
-	GOLANG_ORG_FULL_URL = "https://tip.golang.org/blog/all"
-	GOLANG_ORG_URL      = "https://tip.golang.org"
+	GOLANG_ORG_URL = "https://tip.golang.org"
 )
 
 type GolangOrgHandler struct {
@@ -20,8 +19,20 @@ type GolangOrgHandler struct {
 	err      error
 }
 
-func NewGolangOrgHandler(aURL string) GolangOrgHandler {
-	return GolangOrgHandler{url: aURL, articles: make([]core.Article, 0)}
+func NewGolangOrgHandler(aUrl string) GolangOrgHandler {
+	return GolangOrgHandler{url: aUrl, articles: make([]core.Article, 0)}
+}
+
+func (h *GolangOrgHandler) getDescription(aSelection *goquery.Selection) string {
+	lDesc := aSelection.Next()
+	if len(lDesc.Nodes) == 1 {
+		lClass, lOk := lDesc.Attr("class")
+		if lOk && lClass == "blogsummary" {
+			return strings.TrimSpace(lDesc.Text())
+		}
+	}
+
+	return ""
 }
 
 func (h *GolangOrgHandler) Run() []core.Article {
@@ -35,7 +46,7 @@ func (h *GolangOrgHandler) GolangOrgScraper() ([]core.Article, error) {
 	lArticle := core.Article{}
 	lArticles := make([]core.Article, 0, 0)
 
-	resp, lErr := http.Get(GOLANG_ORG_FULL_URL)
+	resp, lErr := http.Get(h.url)
 	if lErr != nil {
 		// TODO: write error to log
 		h.err = errors.New("http request returns an error: ")
@@ -47,53 +58,51 @@ func (h *GolangOrgHandler) GolangOrgScraper() ([]core.Article, error) {
 		fmt.Println("Status code: ", resp.StatusCode)
 	}
 
-	//rewrite the code with goquery, without goColly
 	doc, lErr := goquery.NewDocumentFromReader(resp.Body)
 	if lErr != nil {
 		// TODO: write error to log
 		h.err = errors.New("goquery.NewDocumentFromReader returns an error: ")
 	}
 
-	doc.Find("p.blogtitle, p.blogsummary").Each(func(i int, selection *goquery.Selection) {
-		if i%2 == 0 {
-			lLink, _ := selection.Find("a").Attr("href")
-			lT, lErr := core.ParseDate("_2 January 2006", selection.Find("span.date").Text())
-			if lErr != nil {
-				// TODO: write error to log
-				h.err = errors.New("error of ParseDate function: ")
-			}
-
-			//title is a required field
-			lArticle.Title = selection.Find("a[href]").Text()
-			if len(lArticle.Title) == 0 {
-				// TODO: write error to log
-				h.err = errors.New("title not found in article ==> exit")
-				//how to return h.err
-			}
-
-			lArticle.Link = GOLANG_ORG_URL + lLink
-			if len(lLink) == 0 {
-				// TODO: write error to log
-				h.err = errors.New("link not found in article ==> exit")
-				//how to return h.err
-			}
-
-			lArticle.Author = selection.Find("span.author").Text()
-			if len(lArticle.Author) == 0 {
-				// TODO: write error to log
-				h.err = errors.New("author not found in article")
-			}
-
-			lArticle.PublishDate = lT
-
-			lArticle.Description = strings.TrimSpace(selection.NextFiltered("p.blogsummary").Text())
-			if len(lArticle.Description) == 0 {
-				// TODO: write error to log
-				h.err = errors.New("description not found in article")
-			}
-
-			lArticles = append(lArticles, lArticle)
+	// doc.Find("p.blogtitle").Each(func(aIndex int, aSelection *goquery.Selection) {
+	doc.Find("p.blogtitle").Each(func(aIndex int, aSelection *goquery.Selection) {
+		lLink, _ := aSelection.Find("a").Attr("href")
+		lT, lErr := core.ParseDate("_2 January 2006", aSelection.Find("span.date").Text())
+		if lErr != nil {
+			// TODO: write error to log
+			h.err = errors.New("error of ParseDate function: ")
 		}
+
+		//title is a required field
+		lArticle.Title = aSelection.Find("a[href]").Text()
+		if len(lArticle.Title) == 0 {
+			// TODO: write error to log
+			h.err = errors.New("title not found in article ==> exit")
+			//how to return h.err
+		}
+
+		lArticle.Link = GOLANG_ORG_URL + lLink
+		if len(lLink) == 0 {
+			// TODO: write error to log
+			h.err = errors.New("link not found in article ==> exit")
+			//how to return h.err
+		}
+
+		lArticle.Author = aSelection.Find("span.author").Text()
+		if len(lArticle.Author) == 0 {
+			// TODO: write error to log
+			h.err = errors.New("author not found in article")
+		}
+
+		lArticle.PublishDate = lT
+
+		lArticle.Description = h.getDescription(aSelection)
+		if len(lArticle.Description) == 0 {
+			// TODO: write error to log
+			h.err = errors.New("description not found in article")
+		}
+
+		lArticles = append(lArticles, lArticle)
 	})
 
 	return lArticles, nil
