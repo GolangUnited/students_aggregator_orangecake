7f5cfc30a5b8731e62d468d0b7b836a49f2a02ce
Abyss <secretkeeper.ik@gmail.com>
2022-11-07 12:17:10 +0300

    Added check on empty struct

--- pkg/configs/server.go
+++ pkg/configs/server.go
@@ -1,13 +1,24 @@
 package configs
 
-import "os"
+import (
+	"os"
+	"github.com/indikator/aggregator_orange_cake/pkg/core"
+)
 
 type Server struct {
 	Port string
 }
 
-func NewServerConfig() *Server {
-	port := os.Getenv("OC_SERVER_PORT")
+func NewServerConfig() (*Server, error) {
+	lConfig := Server{
+		Port: os.Getenv("OC_SERVER_PORT"),
+	}
 
-	return &Server{Port: port}
+	lEmptyConfig := Server{}
+
+	if lConfig == lEmptyConfig {
+		return nil, core.ErrEmptyConfig
+	}
+
+	return &lConfig, nil
 }
