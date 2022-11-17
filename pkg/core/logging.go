51085b181629d05fa1f92fa225af5561c260fe39
Yahor Bagatyrevich <egorbgtrvch@gmail.com>
2022-11-17 15:38:31 +0300

    Moved the logger implementation with its dependencies out of core to its own package.

--- pkg/core/logging.go
+++ pkg/core/logging.go
@@ -1,75 +1,20 @@
 package core
 
 import (
-	"fmt"
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
-	"io"
+	"github.com/indikator/aggregator_orange_cake/pkg/logger"
 	"os"
-	"time"
 )
 
 type Logger interface {
-	Trace(msg string, keyvals ...Value)
-	Info(msg string, keyvals ...Value)
-	Debug(msg string, keyvals ...Value)
-	Warning(msg string, keyvals ...Value)
-	Error(msg string, err error, keyvals ...Value)
+	Trace(msg string, keyvals ...Field)
+	Info(msg string, keyvals ...Field)
+	Debug(msg string, keyvals ...Field)
+	Warning(msg string, keyvals ...Field)
+	Error(msg string, err error, keyvals ...Field)
 }
 
-type gLogger struct {
-	Log zerolog.Logger
-}
+var _ Logger = (*logger.GLogger)(nil)
 
-type Value struct {
-	Key     string
-	Payload interface{}
-}
+type Field = logger.Field
 
-func newGLogger(writers ...io.Writer) gLogger {
-	multi := zerolog.MultiLevelWriter(writers...)
-	w := zerolog.ConsoleWriter{
-		Out:        multi,
-		TimeFormat: time.RFC822,
-		PartsOrder: []string{"level", "time", "message"},
-	}
-	return gLogger{log.Output(w)}
-}
-
-func (g *gLogger) Trace(msg string, keyvals ...Value) {
-	event := g.Log.Trace()
-	addFields(event, keyvals...)
-	event.Msg(msg)
-}
-
-func (g *gLogger) Info(msg string, keyvals ...Value) {
-	event := g.Log.Info()
-	addFields(event, keyvals...)
-	event.Msg(msg)
-}
-
-func (g *gLogger) Debug(msg string, keyvals ...Value) {
-	event := g.Log.Debug()
-	addFields(event, keyvals...)
-	event.Msg(msg)
-}
-
-func (g *gLogger) Warning(msg string, keyvals ...Value) {
-	event := g.Log.Warn()
-	addFields(event, keyvals...)
-	event.Msg(msg)
-}
-
-func (g *gLogger) Error(msg string, err error, keyvals ...Value) {
-	event := g.Log.Error().Err(err)
-	addFields(event, keyvals...)
-	event.Msg(msg)
-}
-
-func addFields(e *zerolog.Event, keyvals ...Value) {
-	for _, value := range keyvals {
-		e = e.Interface(fmt.Sprintf("%s", value.Key), value.Payload)
-	}
-}
-
-var logger = newGLogger(os.Stdout)
+var GLogger = logger.NewGLogger(os.Stdout)
---
+++ pkg/logger/logger.go
@@ -0,0 +1,64 @@
+package logger
+
+import (
+	"fmt"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"io"
+	"time"
+)
+
+type GLogger struct {
+	Log zerolog.Logger
+}
+
+type Field struct {
+	Key   string
+	Value interface{}
+}
+
+func NewGLogger(writers ...io.Writer) GLogger {
+	multi := zerolog.MultiLevelWriter(writers...)
+	w := zerolog.ConsoleWriter{
+		Out:        multi,
+		TimeFormat: time.RFC822,
+		PartsOrder: []string{"level", "time", "message"},
+	}
+	return GLogger{log.Output(w)}
+}
+
+func (g *GLogger) Trace(msg string, keyvals ...Field) {
+	event := g.Log.Trace()
+	addFields(event, keyvals...)
+	event.Msg(msg)
+}
+
+func (g *GLogger) Info(msg string, keyvals ...Field) {
+	event := g.Log.Info()
+	addFields(event, keyvals...)
+	event.Msg(msg)
+}
+
+func (g *GLogger) Debug(msg string, keyvals ...Field) {
+	event := g.Log.Debug()
+	addFields(event, keyvals...)
+	event.Msg(msg)
+}
+
+func (g *GLogger) Warning(msg string, keyvals ...Field) {
+	event := g.Log.Warn()
+	addFields(event, keyvals...)
+	event.Msg(msg)
+}
+
+func (g *GLogger) Error(msg string, err error, keyvals ...Field) {
+	event := g.Log.Error().Err(err)
+	addFields(event, keyvals...)
+	event.Msg(msg)
+}
+
+func addFields(e *zerolog.Event, keyvals ...Field) {
+	for _, field := range keyvals {
+		e = e.Interface(fmt.Sprintf("%s", field.Key), field.Value)
+	}
+}
