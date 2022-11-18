b7269e69fd969caad94d3f9c524cf96be27b14c8
Artem G <itemun@gmail.com>
2022-11-18 09:18:17 +0300

    Add err to ReadArticles & add validation

--- internal/repository/sqlite/articles.go
+++ internal/repository/sqlite/articles.go
@@ -16,15 +16,34 @@ func NewStorage(db *gorm.DB) *SqliteStorage {
 }
 
 // NewTable create table articles with field like struct core.ArticleDB
-func NewTable(db *gorm.DB) {
+func (s *SqliteStorage) NewTable(db *gorm.DB) {
 	lErr := db.AutoMigrate(&core.ArticleDB{})
 	if lErr != nil {
 		panic("failed to migrate from Article struct")
 	}
 }
 
+func cut(aText string, aLimit int) string {
+	lRunes := []rune(aText)
+	if len(lRunes) >= aLimit {
+		return string(lRunes[:aLimit])
+	}
+	return aText
+}
+
 func (s *SqliteStorage) WriteArticles(lArticles []core.Article) error {
 	for _, lArticle := range lArticles {
+		//Validation length of fields
+		if len(lArticle.Title) > 300 {
+			lArticle.Title = cut(lArticle.Title, 300)
+		}
+		if len(lArticle.Author) > 200 {
+			lArticle.Author = cut(lArticle.Author, 200)
+		}
+		if len(lArticle.Description) > 6000 {
+			lArticle.Description = cut(lArticle.Description, 6000)
+		}
+
 		s.db.Create(&core.ArticleDB{
 			Title:       lArticle.Title,
 			Author:      lArticle.Author,
@@ -39,11 +58,11 @@ func (s *SqliteStorage) WriteArticles(lArticles []core.Article) error {
 func (s *SqliteStorage) ReadArticleByID(aID uint) (*core.ArticleDB, error) {
 	var lArticle core.ArticleDB
 
-	lResult := s.db.Where("id = ?", aID).Find(&lArticle)
+	lResult := s.db.Where("id = ?", aID).First(&lArticle)
 
 	lErr := lResult.Error
 	if lErr != nil {
-		fmt.Errorf("Error row with id = %d: %w", aID, lErr)
+		fmt.Errorf("Error happens in row with id = %d: %w", aID, lErr)
 		return nil, lErr
 	}
 
@@ -52,9 +71,14 @@ func (s *SqliteStorage) ReadArticleByID(aID uint) (*core.ArticleDB, error) {
 
 func (s *SqliteStorage) ReadArticlesByDateRange(aMin, aMax time.Time) ([]core.ArticleDB, error) {
 	lArticles := make([]core.ArticleDB, 0)
-	//TODO Wrap errors
-	s.db.Where("publish_date BETWEEN ? AND ?", aMin, aMax).Find(&lArticles)
 
+	lResult := s.db.Where("publish_date BETWEEN ? AND ?", aMin, aMax).Find(&lArticles)
+	lErr := lResult.Error
+	if lErr != nil {
+		fmt.Errorf("Error happens in rows between dates %s and %s: %w", aMin, aMax, lErr)
+		return nil, lErr
+	}
+	fmt.Printf("%d rows was found.", lResult.RowsAffected)
 	return lArticles, nil
 }
 
