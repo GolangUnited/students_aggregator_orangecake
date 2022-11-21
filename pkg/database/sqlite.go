1b542ee8cbd688d10e64d7f6dd03a57c5791112a
Artem G <itemun@gmail.com>
2022-11-21 12:49:07 +0300

    Add ping

--- pkg/database/sqlite.go
+++ pkg/database/sqlite.go
@@ -1,21 +1,22 @@
 package database
 
 import (
+	"database/sql"
 	"fmt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-func NewSqliteConnection(aFileName string) (*gorm.DB, error) {
+func NewSqliteConnection(aFileName string) (*sql.DB, error) {
 	db, lErr := gorm.Open(sqlite.Open(aFileName), &gorm.Config{})
 	if lErr != nil {
 		return nil, fmt.Errorf("can't open database: %w", lErr)
 	}
 
-	//TODO Ping in GORM
-	/*	if err := db.Ping(); err != nil {
-		return nil, fmt.Errorf("can't connect to database: %w", err)
-	}*/
+	sqlDb, lErr := db.DB()
 
-	return db, nil
+	if err := sqlDb.Ping(); err != nil {
+		return nil, fmt.Errorf("can't connect to database: %w", err)
+	}
+	return sqlDb, nil
 }
