fc90bac86a58dd4bf3a6aa2a13eea47375ccb69f
Artem G <itemun@gmail.com>
2022-11-25 17:05:48 +0300

    tiny fix in arg

--- pkg/database/sqlite.go
+++ pkg/database/sqlite.go
@@ -1,13 +1,12 @@
 package database
 
 import (
-	"database/sql"
 	"fmt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-func NewSqliteConnection(aFileName string) (*sql.DB, error) {
+func NewSqliteConnection(aFileName string) (*gorm.DB, error) {
 	db, lErr := gorm.Open(sqlite.Open(aFileName), &gorm.Config{})
 	if lErr != nil {
 		return nil, fmt.Errorf("can't open database: %w", lErr)
@@ -18,5 +17,5 @@ func NewSqliteConnection(aFileName string) (*sql.DB, error) {
 	if err := sqlDb.Ping(); err != nil {
 		return nil, fmt.Errorf("can't connect to database: %w", err)
 	}
-	return sqlDb, nil
+	return db, nil
 }
