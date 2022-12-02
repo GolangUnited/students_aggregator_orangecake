ad4351d5dae85f4a385827b7e9cf77c45cb055db
Artem G <itemun@gmail.com>
2022-12-02 09:08:19 +0300

    Set silent to GORM logger

--- pkg/database/sqlite.go
+++ pkg/database/sqlite.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func NewSqliteConnection(aFileName string) (*gorm.DB, error) {
-	db, lErr := gorm.Open(sqlite.Open(aFileName), &gorm.Config{})
+	db, lErr := gorm.Open(sqlite.Open(aFileName), &gorm.Config{Logger: logger.Default.LogMode(logger.Silent)})
 	if lErr != nil {
 		return nil, fmt.Errorf("can't open database: %w", lErr)
 	}
