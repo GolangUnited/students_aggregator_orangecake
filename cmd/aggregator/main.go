b3996441167124dc185f76516ad014ec7179440b
Artem G <itemun@gmail.com>
2022-12-10 00:39:08 +0300

    Add ReadArticleByID

--- cmd/aggregator/main.go
+++ cmd/aggregator/main.go
@@ -26,6 +26,18 @@ func main() {
 
 	// write articles from handler
 	if lErr := lStorage.WriteArticles(lArticles); lErr != nil {
-		fmt.Printf("error of write articles: %v", lErr)
+		fmt.Printf("error of WriteArticles: %v", lErr)
 	}
+
+	//read article with id = 1 from database
+	lArticle, lErr := lStorage.ReadArticleByID(1)
+	if lErr != nil {
+		fmt.Printf("error of ReadArticleByID: %v", lErr)
+	}
+
+	fmt.Printf("Node %d: %s\n", lArticle.ID, lArticle.Article.Title)
+	fmt.Printf("  Author: %s\n", lArticle.Article.Author)
+	fmt.Printf("  Date: %s\n", lArticle.Article.PublishDate.Format("Jan _2, 2006"))
+	fmt.Printf("  URL: %s\n", lArticle.Article.Link)
+	fmt.Printf("  Description:\n    %s\n\n", lArticle.Article.Description)
 }
