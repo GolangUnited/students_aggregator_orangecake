69509b503adcbda7513fd08f9d595d3da743d42c
Yahor Bagatyrevich <egorbgtrvch@gmail.com>
2022-12-11 17:09:48 +0300

    Rewrote the prints to the calls of the logger

--- cmd/aggregator/main.go
+++ cmd/aggregator/main.go
@@ -15,15 +15,21 @@ func main() {
 	var lArticles []core.Article
 
 	for i, lArticle := range lArticles {
-		fmt.Printf("Node %d: %s\n", i, lArticle.Title)
-		fmt.Printf("  Author: %s\n", lArticle.Author)
-		fmt.Printf("  Date: %s\n", lArticle.PublishDate.Format("Jan _2, 2006"))
-		fmt.Printf("  URL: %s\n", lArticle.Link)
-		fmt.Printf("  Description:\n    %s\n\n", lArticle.Description)
+		articleDescr := fmt.Sprintf("Node %d: %s\n"+
+			"  Author: %s\n"+
+			"  Date: %s\n"+
+			"  URL: %s\n"+
+			"  Description:\n    %s\n\n",
+			i, lArticle.Title,
+			lArticle.Author,
+			lArticle.PublishDate.Format("Jan _2, 2006"),
+			lArticle.Link,
+			lArticle.Description)
+		logger.Info(articleDescr)
 	}
 
-	fmt.Println()
-	fmt.Println()
-	fmt.Printf("  %d Articles detected.", len(lArticles))
-	fmt.Println()
+	logger.Info("")
+	logger.Info("")
+	logger.Info(fmt.Sprintf("  %d Articles detected.", len(lArticles)))
+	logger.Info("")
 }
