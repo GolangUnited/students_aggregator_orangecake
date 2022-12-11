a4f51e1346d2ab5ba7f5d238f03afdfd308129ff
Yahor Bagatyrevich <egorbgtrvch@gmail.com>
2022-12-11 15:39:14 +0300

    Corrected logger to the interface, wrote unit tests for it. Added the creation of logger in main.go.

--- cmd/aggregator/main.go
+++ cmd/aggregator/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	_ "github.com/PuerkitoBio/goquery" // temporary import to init go.mod and go.sum and avoid compiler errors
 	"github.com/indikator/aggregator_orange_cake/pkg/core"
 )
 
 func main() {
+	var logger core.Logger = core.NewZeroLogger(os.Stdout)
+	logger.Info("Starting the aggregator's work.")
+
 	var lArticles []core.Article
 
 	for i, lArticle := range lArticles {
---
+++ pkg/core/logger.go
@@ -0,0 +1,59 @@
+package core
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"io"
+	"strconv"
+	"time"
+)
+
+type zeroLogger struct {
+	log zerolog.Logger
+}
+
+func NewZeroLogger(aWriters ...io.Writer) Logger {
+	lMulti := zerolog.MultiLevelWriter(aWriters...)
+	w := zerolog.ConsoleWriter{
+		Out:        lMulti,
+		TimeFormat: time.RFC822,
+		PartsOrder: []string{"level", "time", "message"},
+	}
+	return zeroLogger{log.Output(w)}
+}
+
+func (zero zeroLogger) Trace(aMessage string, aValues ...interface{}) {
+	lEvent := zero.log.Trace()
+	addFields(lEvent, aValues...)
+	lEvent.Msg(aMessage)
+}
+
+func (zero zeroLogger) Info(aMessage string, aValues ...interface{}) {
+	lEvent := zero.log.Info()
+	addFields(lEvent, aValues...)
+	lEvent.Msg(aMessage)
+}
+
+func (zero zeroLogger) Debug(aMessage string, aValues ...interface{}) {
+	lEvent := zero.log.Debug()
+	addFields(lEvent, aValues...)
+	lEvent.Msg(aMessage)
+}
+
+func (zero zeroLogger) Warn(aMessage string, aValues ...interface{}) {
+	lEvent := zero.log.Warn()
+	addFields(lEvent, aValues...)
+	lEvent.Msg(aMessage)
+}
+
+func (zero zeroLogger) Error(aMessage string, aValues ...interface{}) {
+	lEvent := zero.log.Error()
+	addFields(lEvent, aValues...)
+	lEvent.Msg(aMessage)
+}
+
+func addFields(e *zerolog.Event, aValues ...interface{}) {
+	for i, value := range aValues {
+		e = e.Interface("v"+strconv.Itoa(i), value)
+	}
+}
