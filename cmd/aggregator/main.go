e475678cb743b286487b95a605bea208581babd6
Abyss <secretkeeper.ik@gmail.com>
2022-12-14 21:22:46 +0300

    Modified

--- cmd/aggregator/main.go
+++ cmd/aggregator/main.go
@@ -2,30 +2,30 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"github.com/indikator/aggregator_orange_cake/pkg/core"
 	"github.com/indikator/aggregator_orange_cake/pkg/handlers"
+	"os"
 )
 
-type ScrapperConstruct func(string, *core.Logger) interface{}
+type ScrapperConstruct func(string, core.Logger) core.ArticleScraper
 
 var ScrappersMap = map[string]ScrapperConstruct{
-	"devto": func(url string, logger *core.Logger) interface{} {
-		return handlers.NewDevtoHandler(url /*TODO add logger to constructor*/)
+	"devto": func(url string, logger core.Logger) core.ArticleScraper {
+		return nil //handlers.NewDevtoHandler(url /*TODO add logger to constructor*/)
 	},
-	"bitfield": func(url string, logger *core.Logger) interface{} {
-		return handlers.NewBitfieldScrapper(url /*TODO add logger to constructor*/)
+	"bitfield": func(url string, logger core.Logger) core.ArticleScraper {
+		return handlers.NewBitfieldScrapper(url, logger)
 	},
-	"threedotslabs": func(url string, logger *core.Logger) interface{} {
+	"threedotslabs": func(url string, logger core.Logger) core.ArticleScraper {
 		return handlers.NewThreeDotsLabsHandler(url /*TODO add logger to constructor*/)
 	},
-	"hashnode": func(url string, logger *core.Logger) interface{} {
+	"hashnode": func(url string, logger core.Logger) core.ArticleScraper {
 		return nil //handlers.NewHashnodeScraper(&log.Logger{} /*TODO change log.logger to core.logger*/, url)
 	},
-	"appliedgo": func(url string, logger *core.Logger) interface{} {
-		return handlers.NewAppliedGoScraper(url, *logger)
+	"appliedgo": func(url string, logger core.Logger) core.ArticleScraper {
+		return handlers.NewAppliedGoScraper(url, logger)
 	},
-	"golangorg": func(url string, logger *core.Logger) interface{} {
+	"golangorg": func(url string, logger core.Logger) core.ArticleScraper {
 		return handlers.NewGolangOrgHandler(url /*TODO add logger to constructor*/)
 	},
 }
@@ -36,12 +36,13 @@ func main() {
 
 	lConfig, err := NewAggregatorConfig()
 	if err != nil {
-		fmt.Println(err)
+		logger.Error(err.Error())
+		return
 	}
 
-	scrappers := CreateScrappers(lConfig, nil)
+	scrappers := CreateScrappers(lConfig, logger)
 
-	lArticles, lWarnings := GetArticles(scrappers, nil)
+	lArticles, _ := GetArticles(scrappers, logger)
 
 	for i, lArticle := range lArticles {
 		lArticleDescr := fmt.Sprintf("Article %d: %s\n", i, lArticle.Title)
@@ -53,36 +54,26 @@ func main() {
 		logger.Info(lArticleDescr)
 	}
 
-	logger.Info("\n\n")
 	logger.Info(fmt.Sprintf("  %d Articles detected.\n", len(lArticles)))
-	fmt.Println("----WARNINGS----")
-	fmt.Println(lWarnings)
 }
 
-func CreateScrappers(aConfig *AggregatorConfig, aLogger *core.Logger) []core.ArticleScraper {
+func CreateScrappers(aConfig *AggregatorConfig, aLogger core.Logger) []core.ArticleScraper {
 	lScrappers := make([]core.ArticleScraper, 0)
 	for _, value := range aConfig.Handlers {
 		scrapper := ScrappersMap[value.Handler](value.URL, aLogger)
-		result, ok := scrapper.(core.ArticleScraper)
-		if !ok {
-			//TODO log
-			fmt.Println(fmt.Errorf("%s, %w", value.Handler, core.ErrUnableCastToInterface))
-			continue
-		}
-		lScrappers = append(lScrappers, result)
+		lScrappers = append(lScrappers, scrapper)
 	}
 
 	return lScrappers
 }
 
-func GetArticles(aScrappers []core.ArticleScraper, logger *core.Logger) ([]core.Article, []core.Warning) {
+func GetArticles(aScrappers []core.ArticleScraper, logger core.Logger) ([]core.Article, []core.Warning) {
 	lArticles := make([]core.Article, 0)
 	lWarnings := make([]core.Warning, 0)
 	for _, scrapper := range aScrappers {
 		articles, warnings, err := scrapper.ParseArticles()
 		if err != nil {
-			//TODO log
-			fmt.Println(err)
+			logger.Warn(err.Error())
 			continue
 		}
 		lArticles = append(lArticles, articles...)
